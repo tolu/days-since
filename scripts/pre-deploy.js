26db5617994cb765d0d394b17a8253f1516609ed
Tobias Lundin <tobias.lundin@gmail.com>
2019-09-03 00:19:49 +0200

    fix pre-deplot script

--- scripts/pre-deploy.js
+++ scripts/pre-deploy.js
@@ -2,13 +2,14 @@
 const fs = require('fs');
 const path = require('path');
 
+console.log('Rewrite index.html for github pages...');
+const indexHtml = path.join(__dirname, '../public/index.html');
+const content = fs.readFileSync(indexHtml, { encoding: 'utf8' });
+const output = content.replace(/'\//g, `'/days-since/`);
+console.log(output);
+
 if (process.env['GITHUB_ACTIONS']) {
-  console.log('Rewrite index.html for github pages...');
-  const indexHtml = path.join(__dirname, '../public/index.html');
-  const content = fs.readFileSync(indexHtml, { encoding: 'utf8' });
-  const output = content.replace(/"\//g, '"');
-  console.log(output);
   fs.writeFileSync(indexHtml, output);
 } else {
-  console.log('Only runs inside github action...');
+  console.log('\nFile not written outside github action...');
 }
